4f6cbab8652837660b150ebaec30236a9d00d4a5
Mehdi Allahyari <mallahyari@georgiasouthern.edu>
2020-04-14 22:52:12 -0400

    tfidf is added

--- ml_tutorial/_nbdev.py
+++ ml_tutorial/_nbdev.py
@@ -22,14 +22,24 @@ index = {"flip_coin": "01_mle_map.ipynb",
          "load_symbols": "06_cnn.ipynb",
          "get_pixel": "07_autoencoder.ipynb",
          "change_image_background": "07_autoencoder.ipynb",
-         "create_augmentor_pipeline": "07_autoencoder.ipynb"}
+         "create_augmentor_pipeline": "07_autoencoder.ipynb",
+         "preprocess": "08_qa_tfidf.ipynb",
+         "create_tfidf_features": "08_qa_tfidf.ipynb",
+         "calculate_similarity": "08_qa_tfidf.ipynb",
+         "show_similar_documents": "08_qa_tfidf.ipynb",
+         "create_index": "08_qa_tfidf.ipynb",
+         "index_data": "08_qa_tfidf.ipynb",
+         "index_batch": "08_qa_tfidf.ipynb",
+         "run_query_loop": "08_qa_tfidf.ipynb",
+         "handle_query": "08_qa_tfidf.ipynb"}
 
 modules = ["mle_map.py",
            "linear_regression.py",
            "logistic_regression.py",
            "neural_network.py",
            "cnn.py",
-           "autoencoder.py"]
+           "autoencoder.py",
+           "qa_tfidf.py"]
 
 doc_url = "https://sci2lab.github.io/ml_tutorial/"
 
